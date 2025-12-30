9929bd3da00b51993cf8dda0aad6d31c93ddc8ef
Tharun Ganapathi <tharunganapathik@gmail.com>
2025-12-30 20:41:51 +0530

    Initial commit for Zenith microservices

---
+++ reconcile-service/src/main/java/com/zenithpay/reconcile_service/ReconcileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zenithpay.reconcile_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReconcileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReconcileServiceApplication.class, args);
+	}
+
+}
---
+++ reconcile-service/src/test/java/com/zenithpay/reconcile_service/ReconcileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenithpay.reconcile_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReconcileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
